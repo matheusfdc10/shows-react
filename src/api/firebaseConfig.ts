fff0ba46fd307f4941fb66e9ad9c0afcf69b24c6
matheusfdc10 <64614285+matheusfdc10@users.noreply.github.com>
2023-04-04 16:37:30 -0300

    up

--- src/api/firebaseConfig.ts
+++ src/api/firebaseConfig.ts
@@ -3,10 +3,17 @@
 // dotenv.config();
 
 export default {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID
+   apiKey: "AIzaSyC28pSCUjf3YVXNSkIVIpm1WvO71poEFvM",
+   authDomain: "shows-ad688.firebaseapp.com",
+   projectId: "shows-ad688",
+   storageBucket: "shows-ad688.appspot.com",
+   messagingSenderId: "434489405755",
+   appId: "1:434489405755:web:cc9a805f3070dbfb4c071e"
+
+  // apiKey: process.env.APIKEY,
+  // authDomain: process.env.AUTHDOMAIN,
+  // projectId: process.env.PROJECTID,
+  // storageBucket: process.env.STORAGEBUCKET,
+  // messagingSenderId: process.env.MESSAGINGSENDERID,
+  // appId: process.env.APPID
 };
